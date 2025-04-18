5a99ab9b7de94b066a6766893934c5c1f050ad5f
thxhub0 <lunadevthx@gmail.com>
2025-04-18 15:42:26 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,27 +1,21 @@
- if game.PlaceId == 76598287484083 then 
-
 local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/discoart/FluentPlus/refs/heads/main/release.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
-
-
-
---ISSO DAKI É A UI VC MODIFCA COMO QUISER
-
+     
 local Window = Fluent:CreateWindow({
     Title = "thx",
     SubTitle = "pelo thx",
-    TabWidth = 100, -- ISSO DAKI É BAGULHO DAS TAB
-    Size = UDim2.fromOffset(330, 300),-- TAMANHO DA UI
-    Acrylic = false,-- IGNORA
-    Theme = "Dark", -- THEMA PADRAO 
-    MinimizeKey = Enum.KeyCode.LeftControl -- ISSO DAKI É A LETRA QUE VAI MINIMIZAR NO PC
+    TabWidth = 100, 
+    Size = UDim2.fromOffset(330, 300,)
+    Acrylic = false,
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl 
 })
-local Tabs = {-- as tabs, vc cria como quiser
+local Tabs = {
     Main = Window:AddTab({ Title = "• Infor", Icon = "rbxassetid://18831448204" }),
     Settings = Window:AddTab({ Title = "• Settings", Icon = "rbxassetid://18319394996" }),
 }
- _G.loopAtivo = true
+
 local attack = Tabs.Main2:AddToggle("Auto Click", {Title = "Auto", Default = false})
 
 attack:OnChanged(function()
@@ -30,10 +24,8 @@ attack:OnChanged(function()
         local args = {
             [1] = "Anime Power"
         }
-
-      
+        
         game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("events"):WaitForChild("RemoteEvent")
-      
+        
     end
 end)
-    
