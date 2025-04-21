1aa9f011e125a320cabeed77f631a155345ce0b5
thxhub0 <lunadevthx@gmail.com>
2025-04-21 08:49:33 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -12,39 +12,86 @@ local Window = Fluent:CreateWindow({
     MinimizeKey = Enum.KeyCode.LeftControl 
 })
 local Tabs = {
-    Main = Window:AddTab({ Title = "Auto farm", Icon = "rbxassetid://18831448204" }),
-    Eggs = Window:AddTab({ Title = "• eggs", Icon = "rbxassetid://18319394996" }),
-     Settings = Window:AddTab({ Title = "setting", Icon = "rbxassetid://18831448204" }),
+    Main = Window:AddTab({ Title = "Auto farm", Icon = "rbxassetid://18831448204" }), Eggs = Window:AddTab({ Title = "setting", Icon = "rbxassetid://18831448204" }),
 } 
 Window:SelectTab(1)
 
-
- local attack = Tabs.Main:AddToggle("Auto Click", {Title = "Auto", Default = false})
+ local MagnetToggle = Tabs.Main:AddToggle("MagnetToggle", {Title = "Magnet Drops", Default = false})
+MagnetToggle:OnChanged(function()
+    while MagnetToggle.Value do 
+        wait()
+        local drops = workspace.temp:GetChildren()
+        for _, drop in ipairs(drops) do
+            if drop:IsA("Part") and #drop.Name > 30 then
+                drop.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+            end
+        end
+    end
+end)
+     local attack = Tabs.Main:AddToggle("Main", {Title = "Auto click", Default = false})
 attack:OnChanged(function()
     while attack.Value do
-        wait(0.1)
-     local args = {
+        wait(0.1) 
+   local args = {
     [1] = "attack"
 }
-game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("events"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
-          end
-     end)
 
- local attack = Tabs.Eggs:AddToggle("eggs", {Title = "auto open", Default = false})
+game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("events"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
+         end
+  end)
+     local attack = Tabs.Eggs:AddToggle("Main", {Title = "auto open namek", Default = false})
 attack:OnChanged(function()
-     while attack.Value do
-         wait(0.1)
-      local args = {
+    while attack.Value do
+        wait(0.1) 
+  local args = {
+    [1] = "rollChampion",
+    [2] = "one",
+    [3] = "namek"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("events"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
+       end
+   end)
+    local attack = Tabs.Eggs:AddToggle("Main", {Title = "Auto open boa tavern", Default =  false})
+attack:OnChanged(function()
+    while attack.Value do
+       wait(0.1) 
+   local args = {
+    [1] = "rollChampion",
+    [2] = "one",
+    [3] = "boar tavern"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("events"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
+      end
+  end)
+   local attack = Tabs.Eggs:AddToggle("Main", {Title = "Auto shinobi", Default = false})
+attack:OnChanged(function()
+    while attack.Value do
+        wait(0.1) 
+ local args = {
     [1] = "rollChampion",
     [2] = "one",
     [3] = "shinobi world"
 }
 
 game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("events"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
-    end
+     end
 end)
+  local attack = Tabs.Eggs:AddToggle("Main", {Title = "Auto ilha de páscoa", Default = false})
+attack:OnChanged(function()
+    while attack.Value do
+        wait(0.1) 
+  local args = {
+    [1] = "rollChampion",
+    [2] = "one",
+    [3] = "easter island"
+}
 
-   local attack = Tabs.Main:AddToggle("auto farmi", {Title = "mobs", Default = false})
+game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("events"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
+      end
+end)
+  local attack = Tabs.Main:AddToggle("auto farmi", {Title = "mobs", Default = false})
 attack:OnChanged(function()
      while attack.Value do
          wait(0.1)
@@ -56,16 +103,3 @@ attack:OnChanged(function()
 game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("events"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
      end
 end)
-
- local attack = Tabs.Main:AddToggle("collect", {Title = "collect time", Default = false})
-attack:OnChanged(function()
-     while attack.Value do
-         wait(3.0)
-       local args = {
-    [1] = "collectTimeReward",
-    [2] = 1
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("events"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
-          end
-     end)
