d5986d3c359bf997e22ebc3eb31334842d56a879
thxhub0 <lunadevthx@gmail.com>
2025-05-10 15:11:12 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -12,94 +12,58 @@ local Window = Fluent:CreateWindow({
     MinimizeKey = Enum.KeyCode.LeftControl 
 })
 local Tabs = {
-    Main = Window:AddTab({ Title = "Auto farm", Icon = "rbxassetid://18831448204" }), Eggs = Window:AddTab({ Title = "setting", Icon = "rbxassetid://18831448204" }),
+    Main = Window:AddTab({ Title = "Auto farm", Icon = "rbxassetid://18831448204" }),
+    Eggs = Window:AddTab({ Title = "• eggs", Icon = "rbxassetid://18319394996" }),
+     Settings = Window:AddTab({ Title = "setting", Icon = "rbxassetid://18831448204" }),
 } 
 Window:SelectTab(1)
 
- local MagnetToggle = Tabs.Main:AddToggle("MagnetToggle", {Title = "Magnet Drops", Default = false})
-MagnetToggle:OnChanged(function()
-    while MagnetToggle.Value do 
-        wait()
-        local drops = workspace.temp:GetChildren()
-        for _, drop in ipairs(drops) do
-            if drop:IsA("Part") and #drop.Name > 30 then
-                drop.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-            end
-        end
-    end
-end)
-     local attack = Tabs.Main:AddToggle("Main", {Title = "Auto click", Default = false})
+
+ local attack = Tabs.Main:AddToggle("main", {Title = "Auto open dragon ball", Default = false})
 attack:OnChanged(function()
     while attack.Value do
-        wait(0.1) 
-   local args = {
-    [1] = "attack"
+        wait(0.1)
+local args = {
+	"DragonBall",
+	"Normal",
+	1
 }
-
-game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("events"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
-         end
-  end)
-     local attack = Tabs.Eggs:AddToggle("Main", {Title = "auto open namek", Default = false})
-attack:OnChanged(function()
-    while attack.Value do
-        wait(0.1) 
-  local args = {
-    [1] = "rollChampion",
-    [2] = "one",
-    [3] = "namek"
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("events"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
-       end
-   end)
-    local attack = Tabs.Eggs:AddToggle("Main", {Title = "Auto open boa tavern", Default =  false})
-attack:OnChanged(function()
-    while attack.Value do
-       wait(0.1) 
-   local args = {
-    [1] = "rollChampion",
-    [2] = "one",
-    [3] = "boar tavern"
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("events"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
-      end
-  end)
-   local attack = Tabs.Eggs:AddToggle("Main", {Title = "Auto shinobi", Default = false})
-attack:OnChanged(function()
-    while attack.Value do
-        wait(0.1) 
- local args = {
-    [1] = "rollChampion",
-    [2] = "one",
-    [3] = "shinobi world"
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("events"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_knit@1.5.1"):WaitForChild("knit"):WaitForChild("Services"):WaitForChild("UnitHatchService"):WaitForChild("RF"):WaitForChild("hatch_unit"):InvokeServer(unpack(args))
      end
 end)
-  local attack = Tabs.Eggs:AddToggle("Main", {Title = "Auto ilha de páscoa", Default = false})
+   local attack = Tabs.Main:AddToggle("main", {Title = "Auto click Frozen captain", Default = false})
 attack:OnChanged(function()
     while attack.Value do
-        wait(0.1) 
+        wait(0.1)
   local args = {
-    [1] = "rollChampion",
-    [2] = "one",
-    [3] = "easter island"
+	{
+		workspace:WaitForChild("Worlds"):WaitForChild("DragonBall"):WaitForChild("FriezaCaptain")
+	}
 }
-
-game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("events"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
-      end
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_knit@1.5.1"):WaitForChild("knit"):WaitForChild("Services"):WaitForChild("UnitsInventoryService"):WaitForChild("RF"):WaitForChild("deal_damage"):InvokeServer(unpack(args))
+    end
 end)
-  local attack = Tabs.Main:AddToggle("auto farmi", {Title = "mobs", Default = false})
+  local attack = Tabs.Main:AddToggle("main", {Title = "Auto click frieza boss", Default = false})
 attack:OnChanged(function()
-     while attack.Value do
-         wait(0.1)
-       local args = {
-    [1] = "grimoireAttack",
-    [2] = workspace:WaitForChild("spawners"):WaitForChild("shinobi world"):WaitForChild("cc7057cd-dec8-4312-a3ca-27e34ef47aa3")
+    while attack.Value do
+        wait(0.1)
+  local args = {
+	{
+		workspace:WaitForChild("Worlds"):WaitForChild("DragonBall"):WaitForChild("Frieza")
+	}
 }
-
-game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("events"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_knit@1.5.1"):WaitForChild("knit"):WaitForChild("Services"):WaitForChild("UnitsInventoryService"):WaitForChild("RF"):WaitForChild("deal_damage"):InvokeServer(unpack(args))
+    end
+end)
+   local attack = Tabs.Main:AddToggle("main", {Title = "Auto open bleach", Default = false})
+attack:OnChanged(function()
+    while attack.Value do
+        wait(0.1)
+local args = {
+	"Bleach",
+	"Normal",
+	1
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_knit@1.5.1"):WaitForChild("knit"):WaitForChild("Services"):WaitForChild("UnitHatchService"):WaitForChild("RF"):WaitForChild("hatch_unit"):InvokeServer(unpack(args))
      end
 end)
